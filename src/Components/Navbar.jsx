1252cc2d0d6f0604a2eaa37ea9cb82d462db6073
KManishY <my1000088@gmail.com>
2022-08-24 15:36:15 +0530

    update nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Flex, Button, Box } from "@chakra-ui/react";
 
 const Navbar = () => {
-    return (
-		<div>
-			<Link to="/">Home</Link>
-		</div>
+	return (
+		<Box width="inherit" p="20px">
+			<Flex align="center" justify="space-evenly" gap="0">
+				<Box width="20%" m="0">
+					<Flex justify="center">
+						<Link to="/">
+							<img
+								src="https://static.naukimg.com/s/4/100/i/naukri_Logo.png"
+								alt="Logo"
+							/>
+						</Link>
+					</Flex>
+				</Box>
+
+				<Box width="40%" color="#8292b4" fontSize="20px">
+					<Flex align="center" gap="10">
+						<Link to="/">Jobs</Link>
+						<Link to="/">Companies</Link>
+						<Link to="/">Services</Link>
+						<Link to="/">Resources</Link>
+					</Flex>
+				</Box>
+				<Box width="40%">
+					<Flex align="center" ml="60px" gap="10px">
+						<Link to="/">
+							<Button
+								style={{ borderRadius: "20%" }}
+								variant="outline"
+								size="xs"
+								colorScheme="blue">
+								Login
+							</Button>
+						</Link>
+						<Link to="/">
+							<Button size="xs" color="white" bgColor="#fe7554">
+								Register
+							</Button>
+						</Link>
+						<Link to="/">
+							<Box
+								fontWeight="bold"
+								colorScheme="red"
+								color="#445578">
+								For employers
+							</Box>
+						</Link>
+					</Flex>
+				</Box>
+			</Flex>
+		</Box>
 	);
 };
 
