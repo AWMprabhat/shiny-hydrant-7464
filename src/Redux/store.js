53ab966bbc9ec9ab65ac3bf1a5d1d3b377b1c788
Naresh Rajput <nareshmewada014@gmail.com>
2022-08-26 13:24:29 +0530

    add filter and company data with all the functionality and UI

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -6,10 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import { reducer as AuthReducer } from "./AuthReducer/reducer";
+import { reducer as AppReducer } from "./AppReducer/reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducer = combineReducers({ AuthReducer });
+const rootReducer = combineReducers({ AuthReducer, AppReducer });
 
 const store = legacy_createStore(
   rootReducer,
